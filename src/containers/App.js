6323325eec2254515bbb98812a6cd8622ec228c9
Brian Lee <tzhlee@gmail.com>
2020-03-11 02:08:18 -0500

    Cockpit created

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 	state = {
@@ -46,7 +47,7 @@ class App extends Component {
 
 	render() {
 		let persons = null;
-		let btnClass = '';
+		//let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
@@ -59,24 +60,12 @@ class App extends Component {
 				</div>
 			);
 
-			btnClass = classes.Red;
-		}
-
-		const assignedClasses = [];
-		if (this.state.persons.length <= 2) {
-			assignedClasses.push(classes.red); // classes = ['red']
-		}
-		if (this.state.persons.length <= 1) {
-			assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+			//btnClass = classes.Red;
 		}
 
 		return (
 			<div className={classes.App}>
-				<h1>Hi, I'm a React App</h1>
-				<p className={assignedClasses.join(' ')}>This is really working!</p>
-				<button className={btnClass} onClick={this.togglePersonsHandler}>
-					Toggle Persons
-				</button>
+				<Cockpit showPersons={this.state.showPersons} persons={this.state.persons} />
 				{persons}
 			</div>
 		);
