5f66f289b5a073f932e83c305da86f7208b50c05
Brian Lee <tzhlee@gmail.com>
2020-04-25 23:36:44 -0500

    Aligned center for App JS fixed

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -20,10 +20,10 @@ const cockpit = (props) => {
 		btnClass = classes.Red;
 	}
 
-	if (props.persons.length <= 2) {
+	if (props.personsLength <= 2) {
 		assignedClasses.push(classes.red); // classes = ['red']
 	}
-	if (props.persons.length <= 1) {
+	if (props.personsLength <= 1) {
 		assignedClasses.push(classes.bold); // classes = ['red', 'bold']
 	}
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import withClass from '../hoc/WithClass';
-
+import WithClass from '../hoc/WithClass';
 class App extends Component {
 	constructor(props) {
 		super(props);
 		console.log('[App.js] constructor');
 	}
-
 	state = {
 		persons: [
 			{ id: 'asfa1', name: 'Max', age: 28 },
@@ -20,61 +18,47 @@ class App extends Component {
 		showPersons: false,
 		showCockpit: true
 	};
-
 	static getDerivedStateFromProps(props, state) {
 		console.log('[App.js] getDerivedStateFromProps', props);
 		return state;
 	}
-
 	componentDidMount() {
 		console.log('[App.js] componentDidMount');
 	}
-
 	shouldComponentUpdate(nextProps, nextState) {
 		console.log('[App.js] shouldComponentUpdate');
 		return true;
 	}
-
 	componentDidUpdate() {
 		console.log('[App.js] componentDidUpdate');
 	}
-
 	nameChangedHandler = (event, id) => {
 		const personIndex = this.state.persons.findIndex((p) => {
 			return p.id === id;
 		});
-
 		const person = {
 			...this.state.persons[personIndex]
 		};
-
 		// const person = Object.assign({}, this.state.persons[personIndex]);
-
 		person.name = event.target.value;
-
 		const persons = [ ...this.state.persons ];
 		persons[personIndex] = person;
-
 		this.setState({ persons: persons });
 	};
-
 	deletePersonHandler = (personIndex) => {
 		// const persons = this.state.persons.slice();
 		const persons = [ ...this.state.persons ];
 		persons.splice(personIndex, 1);
 		this.setState({ persons: persons });
 	};
-
 	togglePersonsHandler = () => {
 		const doesShow = this.state.showPersons;
 		this.setState({ showPersons: !doesShow });
 	};
-
 	render() {
 		console.log('[App.js] render');
 		let persons = null;
 		//let btnClass = '';
-
 		if (this.state.showPersons) {
 			persons = (
 				<Persons
@@ -83,12 +67,10 @@ class App extends Component {
 					changed={this.nameChangedHandler}
 				/>
 			);
-
 			//btnClass = classes.Red;
 		}
-
 		return (
-			<withClass classes={classes.App}>
+			<WithClass classes={classes.App}>
 				<button
 					onClick={() => {
 						this.setState({ showCockpit: false });
@@ -99,15 +81,15 @@ class App extends Component {
 					<Cockpit
 						title={this.props.appTitle}
 						showPersons={this.state.showPersons}
-						persons={this.state.persons}
+						//persons={this.state.persons}
+						personsLength={this.state.persons.length}
 						clicked={this.togglePersonsHandler}
 					/>
 				) : null}
 				{persons}
-			</withClass>
+			</WithClass>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 	}
 }
-
 export default App;
